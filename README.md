c5f2f72f0cf754fec96170fe9c8241626fd73cf3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-20 13:22:57 -0400

    new file:   CHANGE.log 	new file:   DivideAndConquer/AndroidManifest.xml 	new file:   DivideAndConquer/README.txt 	new file:   DivideAndConquer/assets/EULA 	new file:   DivideAndConquer/res/drawable/ball.png 	new file:   DivideAndConquer/res/drawable/ballsicon.jpg 	new file:   DivideAndConquer/res/drawable/explosion1.png 	new file:   DivideAndConquer/res/drawable/explosion2.png 	new file:   DivideAndConquer/res/drawable/explosion3.png 	new file:   DivideAndConquer/res/layout/game_over_dialog.xml 	new file:   DivideAndConquer/res/layout/main.xml 	new file:   DivideAndConquer/res/layout/welcome_dialog.xml 	new file:   DivideAndConquer/res/values/strings.xml 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/AnimatingLine.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/Ball.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/BallEngine.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/BallRegion.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/Direction.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/DirectionPoint.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/DivideAndConquerActivity.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/DivideAndConquerView.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/GameOverDialog.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/NewGameCallback.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/Preferences.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/Shape2d.java 	new file:   DivideAndConquer/src/com/google/android/divideandconquer/WelcomeDialog.java 	new file:   LICENSE 	new file:   README.md 	new file:   do.sum.bash 	new file:   ma.bash 	new file:   sha512.sum

---
+++ README.md
@@ -0,0 +1,20 @@
+# [buildAPKs.games](https://github.com/BuildAPKs/buildAPKs.games)
+Android APK app sources that build in [Termux](https://github.com/termux) in Amazon, Android and Chrome. 
+
+[The buildAPKs.games git repository](https://github.com/BuildAPKs/buildAPKs.games) is a git submodule for [buildAPKs](https://github.com/BuildAPKs/buildAPKs).  In order to install it in its' proper place to attempt to make these applications on smartphone, tablet an TV, copy and paste the following into [Termux](https://github.com/termux):
+
+```
+
+cd && git clone https://github.com/BuildAPKs/buildAPKs
+
+~/buildAPKs/setup.buildAPKs.bash
+
+~/buildAPKs/scripts/build/build.games.bash
+
+```
+
+[buildAPKs.games](https://buildapks.github.io/buildAPKs.games)
+
+See [buildAPKs](https://github.com/BuildAPKs/buildAPKs/) for information regarding this project.
+
+<!--#OEM-->
---
+++ ma.bash
@@ -0,0 +1,65 @@
+#!/bin/env bash # Copyright 2019 (c) all rights reserved 
+# by BuildAPKs https://buildapks.github.io
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+
+_SMATRPERROR_() { # run on script error
+	local RV="$?"
+	echo "$RV" ma.bash
+	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
+	exit 147
+}
+
+_SMATRPEXIT_() { # run on exit
+	printf "\\e[?25h\\e[0m"
+	set +Eeuo pipefail 
+	exit
+}
+
+_SMATRPSIGNAL_() { # run on signal
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "ma.bash" "$RV"
+ 	exit 148 
+}
+
+_SMATRPQUIT_() { # run on quit
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "ma.bash" "$RV"
+ 	exit 149 
+}
+
+trap '_SMATRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
+trap _SMATRPEXIT_ EXIT
+trap _SMATRPSIGNAL_ HUP INT TERM 
+trap _SMATRPQUIT_ QUIT 
+
+cd "$JDR"
+_AT_ FatMonkeySmile/android-dice-game 86ba2baca008de3172aa1e1442f0db09614e27e1
+_AT_ ge0rg/gamemasterdice 2b7e3c848111cdd2186efea3b6196c6f62e5cb18
+_AT_ jareens/dudo 4034fd2bf52b28c5b55efd70a181862517dece6c
+_AT_ jogordon/BomberMan-for-Android 27545a47f5a08d93733843a8273fa51f5e747f94
+_AT_ krixisLv/ShootEmOff c7037296481302c8377e45ba8f8fbf2a0d293101
+_AT_ ktom2135/CoreGame cfb94a86e0f17e38f9910c5da6f134aed045d5bc
+_AT_ meoblast001/thugaim f4871f8cebff83b0b029aec9d7e0132dd10b26fc
+# _AT_ madisp/divide-and-conquer 60678a006c234d0c55a7ef6554f2755201b25841
+_AT_ mobilepearls/com.mobilepearls.memory bc469667974bee2b0750e8474a0844999ed87fbc
+_AT_ mobilepearls/com.mobilepearls.react 7587ad8826dd5dd82c540d34d1bb6f2a009e3819
+_AT_ ppelleti/AndFish 6e64e9960402fd98f2ce85c2531606c6388b7bfb
+_AT_ rorist/android-reverse a6741ed9f7cc390a1ab18ae5961d8e4db4f02f05
+_AT_ svn2github beginnginandroidgames2 1a951db9cb0c9a8df2888704a5d980b167955926
+_AT_ VelbazhdSoftwareLLC/Blokish8 81426d948edb17de2afa06a6e6f70291ff4da33b
+_AT_ VelbazhdSoftwareLLC/BrainstonzForAndroid 78c98c772f4a44ab27cd763583e87308bb66785d
+_AT_ VelbazhdSoftwareLLC/FreeKlondike c373e17592b429c3fa9f54620c408e909352484c
+_AT_ VelbazhdSoftwareLLC/FishRingsForAndroid 0e0186af3305394ebc71966238c1a52bd00448bf
+_AT_ VelbazhdSoftwareLLC/HungarianRingsForAndroid 2f078680562a9a39a8927fcc626cdc0d7eb7c31c
+_AT_ VelbazhdSoftwareLLC/Kechi 7f8f33f668b6bdd4f413cfa74f8a1e4b6371b7c9
+_AT_ VelbazhdSoftwareLLC/Politrics f77ba059a6a5308fb5a98673112aa3406e927809
+_AT_ VelbazhdSoftwareLLC/RussianTripleForAndroid 83bbf80b8c23a32e5f9f310f21cb34c282df515b
+_AT_ VelbazhdSoftwareLLC/SvarkaOddsCalculator a7b8faf49531fd22e5d6e76a287e28460db5bc47
+_AT_ VelbazhdSoftwareLLC/TriPeaksSolitaireForAndroid db27b546c7b6c14d612f415a56e1ef32e8701f26
+_AT_ VelbazhdSoftwareLLC/tuty-fruty-slot-for-android d56053e64262afb4b9fe13e2e96bd8d4f6e394cb
+_AT_ zipzapdat/AndroFish d7f45f5873696f9ce5669dcbf3be2600f293b021
+_AT_ zipzapdat/opensudoku-android c45e5c5d4f8225349b2fd61ad6569f289ebdc930
+
+#OEF
